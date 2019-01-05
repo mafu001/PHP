f30379ce991da45e3172accb3a27d225cb03ccf9
Md. Mahfuz Alam <mahfuz00100@gmail.com>
2019-01-05 17:33:19 +0600

    Null Coalesce Operator

---
+++ Operator/1.31-null-coalesce-operator.php
@@ -0,0 +1,14 @@
+<?php
+
+$x = 5;
+$y;
+
+$result = $y ?? $x; // null coalesce operator show output one value that is true
+echo $result;
+echo PHP_EOL; //php end of line
+
+$x = 5;
+$y = 8;
+
+$result = $y ?? $x; // null coalesce operator show output one value that is true
+echo $result;
