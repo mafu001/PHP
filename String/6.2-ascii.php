f28970d12152e3a8f7cd6a1ca822c0b8d91a4d17
Md. Mahfuz Alam <mahfuz00100@gmail.com>
2019-01-09 19:09:20 +0600

    ASCII
    ASCII( American Standard Code for Information Interchange ) is used here with chr( ) and ord( ) function

---
+++ String/6.2-ascii.php
@@ -0,0 +1,12 @@
+<?php
+
+//ASCII (American Standard Code for Information Interchange)
+//------------------------------------------------------------
+
+//chr() function is used to convert a ASCII value to a character
+echo chr(33);
+
+echo PHP_EOL;
+
+//ord() function is used to get the ASCII value of the first character of a string
+echo ord('b');
