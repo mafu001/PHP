ba3d3348efd38dc0f41113a3462588d69c6f886f
Md. Mahfuz Alam <mahfuz00100@gmail.com>
2019-01-12 18:10:07 +0600

    New line to Break
    \n to <br>

---
+++ String/6.10-new-line-to-break.php
@@ -0,0 +1,8 @@
+<?php
+//NEW LINE TO BREAK
+$string = "
+<?php
+Lorem ipsum dolor sit amet \n, consectetur adipisicing \n elit. Optio nobis explicabo animi ut ratione aut itaque vel inventore sint veritatis, earum ipsam neque vitae distinctio molestiae quidem molestias debitis beatae.";
+
+echo nl2br($string); // new line to break, when i open the file in browser then to work \n
+
